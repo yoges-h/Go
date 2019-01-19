1f188374d524f5013ba14d037d9179f86e9784b7
yogesh9586 <40050931+yogesh9586@users.noreply.github.com>
2019-01-19 20:36:18 +0530

    Update range.go

--- range.go
+++ range.go
@@ -6,6 +6,9 @@ import "fmt"
 var pow  = []int{1,2,4,8,16,32,64,128}
 
 func main(){
+	
+	// Defining the Variable v because we have to store the Value of the Output in v
+	
 
 	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n",i,v)
