8027ab75d54c5c5043d1e06628fd70248b6505cf
Yogesh <yogeshcreative1998@gmail.com>
2019-01-19 18:03:42 +0530

    test

--- array.go
+++ array.go
@@ -8,7 +8,7 @@ import "fmt"
 func  main(){
 
 	 var a [2]string // a [2]string is an array of Strings 
-
+// testing will be there 
 	  a[0] = "Hello" // defining the data of array as "Hello"
 	  a[1] = "world" // Defining the data of array as "world"
 
